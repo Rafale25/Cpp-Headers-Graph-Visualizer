8fb612b84b835d3ea0695cf44c649c24f42095ee
Rafale25 <rafale25.dev@gmail.com>
2026-05-29 02:15:03 +0200

    Added argparse + support for .c, .cpp files, + includes with <>

--- main.py
+++ main.py
@@ -2,8 +2,30 @@ import sys
 from pathlib import Path
 from pyvis.network import Network
 import re
+import argparse
 
-src_path = [Path(p) for p in sys.argv[1:]]
+parser = argparse.ArgumentParser(
+            prog='C/C++ Header Graphing',
+            description='Makes a graph from the #include directives',
+            epilog='')
+
+parser.add_argument('-ho', '--header_only', action='store_true', help='Graph only header files')
+parser.add_argument('-all', '--all_includes', action='store_true', help='Also graph files not found in given paths')
+parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output')
+parser.add_argument('folderpath', nargs='+', help='Path of the top folder you want to recursively search in')
+
+args = parser.parse_args()
+
+FILES_MATCH = ('.hpp', '.h')
+if not args.header_only:
+    FILES_MATCH += ('.c', '.cpp')
+
+if (args.verbose):
+    print(f'Header only: {args.header_only}')
+    print(f'paths: {args.folderpath}')
+    print(f'{FILES_MATCH=}')
+
+src_path = [Path(p) for p in args.folderpath]
 
 def iter_src_pathes():
     for p in src_path:
@@ -23,23 +45,31 @@ nodes = set()
 for f in iter_src_pathes():
     if not f.is_file(): continue
 
-    if f.suffix in ('.hpp', '.h'):
+    if f.suffix in FILES_MATCH:
         nodes.add(f.name)
-    # if f.suffix == '.cpp':
-    #     nodes.add(f.name)
+
+if args.verbose:
+    print(f'Files found: {nodes}')
 
 net.add_nodes(nodes)
 
 for f in iter_src_pathes():
     if not f.is_file(): continue
-    if f.suffix not in ('.hpp', '.h'): continue
+    if f.suffix not in FILES_MATCH: continue
 
     text = f.read_text()
-    includes = re.findall(r'#include\s+"(\w+.hpp)"', text)
+    # includes = re.findall(r'#include\s+"(\w+.(?:hpp|h))"', text)
+    includes = re.findall(r'#include\s+(?:"|<)(\S+)(?:"|>)', text)
+
+    if args.verbose:
+        print(f'{f.name}: {includes}')
 
     for include in includes:
+        if not args.all_includes:
+            if not include in nodes:
+                continue
         if not include in nodes:
-            net.add_nodes(include)
+            net.add_node(include)
         net.add_edge(include, f.name)
 
 
